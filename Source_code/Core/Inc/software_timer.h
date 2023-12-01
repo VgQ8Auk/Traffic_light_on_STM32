e83607861eef4f880874a9b2ec7990e015aa982a
QuantumMarshal <quangnhan1411@gmail.com>
2023-12-01 11:28:54 +0700

    Update sofware_timer.h

--- Source_code/Core/Inc/software_timer.h
+++ Source_code/Core/Inc/software_timer.h
@@ -1,17 +1,35 @@
 /*
  * software_timer.h
  *
- *  Created on: Nov 25, 2023
+ *  Created on: Nov 28, 2023
  *      Author: Admin
  */
 
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
+//  ==== TIMER ====
+/*
+ * timer1: Traffic light
+ * timer2:
+ * timer3:
+ * timer4:
+ * timer5:
+ */
+
 extern int timer1_flag;
+extern int timer2_flag;
+extern int timer3_flag;
+extern int timer4_flag;
+extern int timer5_flag;
 
 void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
+void setTimer4(int duration);
+void setTimer5(int duration);
 
 void timerRun();
+
 
 #endif /* INC_SOFTWARE_TIMER_H_ */

919c2ea002c187c3c35691137856eeaf22d9ff77
IAmHeavy <94903160+VgQ8Auk@users.noreply.github.com>
2023-12-02 07:57:51 +0700

    add header files for fsms

---
+++ Source_code/Core/Inc/manual_fsm.h
@@ -0,0 +1,16 @@
+/*
+ * manual_fsm.h
+ *
+ *  Created on: Nov 28, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_MANUAL_FSM_H_
+#define INC_MANUAL_FSM_H_
+
+#include "global.h"
+#include "led_control.h"
+
+void fsm_manual();
+
+#endif /* INC_MANUAL_FSM_H_ */
---
+++ Source_code/Core/Inc/traffic_light_fsm.h
@@ -0,0 +1,16 @@
+/*
+ * traffic_light_fsm.h
+ *
+ *  Created on: Nov 28, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_TRAFFIC_LIGHT_FSM_H_
+#define INC_TRAFFIC_LIGHT_FSM_H_
+
+#include "global.h"
+#include "led_control.h"
+
+void fsm_traffic_light();
+
+#endif /* INC_TRAFFIC_LIGHT_FSM_H_ */
